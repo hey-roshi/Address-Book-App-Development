3ef17930985a27772635357c5c550f8923f82dd5
roshi <srivastavaroshi00@gmail.com>
2025-03-08 11:19:46 +0530

    used lombok to auto-generate getters and setters

--- Address-Book-App/src/main/java/com/development/Address/Book/App/dto/UserDTO.java
+++ Address-Book-App/src/main/java/com/development/Address/Book/App/dto/UserDTO.java
@@ -1,41 +1,11 @@
 package com.development.Address.Book.App.dto;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
     private String name;
     private String address;
     private int pincode;
     private boolean isPermanentAddress;
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public int getPincode() {
-        return pincode;
-    }
-
-    public void setPincode(int pincode) {
-        this.pincode = pincode;
-    }
-
-    public boolean isPermanentAddress() {
-        return isPermanentAddress;
-    }
-
-    public void setPermanentAddress(boolean permanentAddress) {
-        isPermanentAddress = permanentAddress;
-    }
 }
